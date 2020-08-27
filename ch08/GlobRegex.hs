bcaa54f00bb2c815677e38d8538ed6461748d470
Sukbeom Kim <sukbeom.kim@gmail.com>
2020-08-28 01:33:22 +0900

    [upg] Glob Pattern
    Glob 패턴 이용한 패턴매칭 예제 추가

--- ch08/GlobRegex.hs
+++ ch08/GlobRegex.hs
@@ -1,8 +1,14 @@
 -- file: ch08/GlobRegex.hs
-module GlobRegex (globToRegex, matchesGlob) where
+module GlobRegex (globToRegex) where
 
 import Text.Regex.Posix((=~))
 
+{- glob pattern을 이용하여 하스켈에서 정규식을 사용하는 방법에 대해 정리한다.
+glob 패턴은 유닉스 셸이 사용하는 규칙으로 지정된 패턴을 찾을 때 사용하는 것으로서 
+정규식과 비슷하다.
+-}
+
+-- 정규식은 반드시 anchor(^)로 시작함으로써 문장이 시작되는 것을 알려야한다.
 globToRegex cs = '^' : globToRegex' cs ++ "$"
 
 globToRegex' :: String -> String
