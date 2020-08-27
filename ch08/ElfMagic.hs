225a030278898f78d84cc71fad6c09f587216fdc
Sukbeom Kim <sukbeom.kim@gmail.com>
2020-08-28 02:31:45 +0900

    Chapter08 재확인
    - ELF 대신 테스트용 텍스트 파일 이용하도록 예제 코드 수정
    - qualified import 에 대한 설명 추가

--- ch08/ElfMagic.hs
+++ ch08/ElfMagic.hs
@@ -1,18 +1,32 @@
 -- file: ch08/ElfMagic.hs
--- 파일 종류를 식별하는 바이트 시퀀스는 magic number 라고 불린다.
+-- 파일이 ELF object 파일인지 아닌지를 판단하는 간단한 예제이다.
+-- 파일 종류를 식별하는 바이트 시퀀스는 magic number 라고 불리며, 이 코드에서는
+-- magic number를 조사하고 결과에 따라 ELF object 인지를 판단한다.
 
+-- qualified ... as x 는 x를 앞에 붙이고 ...에 정의된 내용을 사용하라는 뜻이
+-- 아니라, Data.ByteString.Lazy가 L로써 qualified 되었다는 의미이다. 예를 들어,
+-- foldr'은 unqualified name이며, Data.List.foldr' 이 qualified name이다.
+-- 명시적으로 qualified 이름을 사용하겠다는 키워드이며, Shortname으로서 as
+-- 키워드를 이용하여 명시적으로 namespace를 정의한다.
 import qualified Data.ByteString.Lazy as L
 
+-- ByteString 모듈은 binary I/O를 위해 만들어진 모듈이다.
+
 hasElfMagic :: L.ByteString -> Bool
-hasElfMagic content = L.take 4 content == elfMagic 
-    where elfMagic = L.pack [0x7f, 0x45, 0x4c, 0x46]
+hasElfMagic content = L.take 4 content == elfMagic
+    where elfMagic = L.pack [0x54, 0x68, 0x69, 0x73]
+    -- where elfMagic = L.pack [0x7f, 0x45, 0x4c, 0x46]
 
 isElfFile :: FilePath -> IO Bool
-isElfFile path = do 
+isElfFile path = do
+    -- L.readFile은 lazy ByteString 함수이며, 파일 데이터를 요구할 때에만
+    -- 읽기작업을 한다. 또한 최대 64 KB 크기 만큼 한번에 읽을 수 있고 현재처럼 4
+    -- 바이트만 읽고자 하는 경우에 매우 효율적으로 동작한다.
     content <- L.readFile path
-    return (hasElfMagic content)    
+    return (hasElfMagic content)
 
-main = do 
+main :: IO ()
+main = do
     inpFile <- getLine
     print $ "input file : " ++ inpFile
     rvalue <- isElfFile inpFile
--- ch08/SumFile.hs
+++ ch08/SumFile.hs
@@ -1,5 +1,23 @@
 -- file: ch08/SumFile.hs
+-- 텍스트 파일의 숫자를 모두 더한 값을 출력한다.
+
+-- 파일에 대한 I/O로 String 타입이 기본이지만 효율적이지 않다.
+-- 리스트의 각 요소들은 각각 개별적으로 할당되어 일정한 오버헤드를 갖기 때문에,
+-- 메모리 소모나 성능에 영향을 끼친다.
 main = do
     contents <- getContents
     print (sumFile contents)
     where sumFile = sum . map read . words
+
+{- sumFile 에 대한 참고 자료
+
+*Main> map read . words $ "1 2 3 4"
+[*** Exception: Prelude.read: no parse
+*Main> map read . words $ "1 2 3 4"  :: [Int]
+[1,2,3,4]
+*Main> map read . words $ "1 2 3 4"  :: [Double]
+[1.0,2.0,3.0,4.0]
+
+read로 변수 읽었을 때 출력되지 않는 exception이 발생하는데, 이에 대해서는 반드시
+어떤 형태로 출력할 것인지에 대한 내용을 전달해줘야 한다.
+-}
